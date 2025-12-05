70a40d854597c3a52e5af2c94e697427dc6d190b
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-12-05 21:42:28 +0800

    点击TAB显示对应列表

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ const App = () => {
 
   // 获取food list，渲染数据列表
   // 1. use Selector
-  const { foodsList } = useSelector(state => state.foods)
+  const { foodsList, activeIndex } = useSelector(state => state.foods)
 
 
   return (
@@ -100,9 +100,9 @@ const App = () => {
           <div className="list-content">
             <div className="goods-list">
               {/* 外卖商品列表 */}
-              {foodsList.map(item => {
+              {foodsList.map((item, index) => {
                 return (
-                  <FoodsCategory
+                  activeIndex === index && <FoodsCategory
                     key={item.tag}
                     // 列表标题
                     name={item.name}
