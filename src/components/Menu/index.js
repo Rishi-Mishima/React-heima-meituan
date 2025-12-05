f94de0d1b6800fd63aa61048480412c8327c5640
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-12-05 21:14:45 +0800

    点击激活左侧menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,16 @@
 import classNames from 'classnames'
 import './index.scss'
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
 
 const Menu = () => {
   // 从 Redux 获取 foodsList
   const { foodsList } = useSelector(state => state.foods)
 
   const menus = foodsList.map(item => ({ tag: item.tag, name: item.name }))
+  const [activeIndex, setActiveIndex] = useState(0)
+
+
   return (
     <nav className="list-menu">
       {/* 添加active类名会变成激活状态 */}
@@ -14,9 +18,9 @@ const Menu = () => {
         return (
           <div
             key={item.tag}
+            onClick={() => setActiveIndex(index)}
             className={classNames(
-              'list-menu-item',
-              'active'
+              'list-menu-item', index === activeIndex && 'active'
             )}
           >
             {item.name}
